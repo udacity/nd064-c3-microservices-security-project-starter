c0570cf8124c3154268e3cfb9ca47bda7e0999c6
Nick <nreva@snap.com>
2021-05-05 09:22:39 -0700

    update payload.sh

--- starter/scripts/payload.sh
+++ starter/scripts/payload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#start monero_cpu_moneropool:latest
+#start monero_cpu_moneropool
 kubectl run --kubeconfig kube_config_cluster.yml moneropool --image=servethehome/monero_cpu_moneropool:latest --replicas=1
-#start xmrpooldotnet
-kubectl run --kubeconfig kube_config_cluster.yml xmrpooldotnet --image=servethehome/monero_cpu_xmrpooldotnet:latest --replicas=1
 #start minergate
 kubectl run --kubeconfig kube_config_cluster.yml minergate --image=servethehome/monero_cpu_minergate:latest --replicas=1
+#start cryptotonight
+kubectl run --kubeconfig kube_config_cluster.yml minergate --image=servethehome/universal_cryptonight:latest --replicas=1
 
 echo "Can you identify the payload(s)?"
